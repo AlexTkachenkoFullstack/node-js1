05b92d85d70e64e36a2b7a7d470f2e722bb19c7b
Alex <117479029+AlexTkachenkoFullstack@users.noreply.github.com>
2023-07-28 15:20:43 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+https://monosnap.com/list/64c3ca8d2d8ccb1876bf09a7
---
+++ contacts.js
@@ -0,0 +1,57 @@
+const fs = require("fs/promises");
+const { nanoid } = require("nanoid");
+const path = require("path");
+const contactsPath = path.join(__dirname, "db/contacts.json");
+
+async function listContacts() {
+  // Возвращает массив контактов.
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contacts = await JSON.parse(data);
+    return contacts;
+  } catch (error) {}
+}
+
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const oneContact = contacts.find((contact) => contact.id === contactId);
+  return oneContact || null;
+}
+
+async function addContact(data) {
+  const contacts = await listContacts();
+  const newContact = { id: nanoid(), ...data };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+}
+
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const deletedContact = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return deletedContact[0];
+}
+
+async function updateContact(id, data) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((contact) => contact.id === id);
+  if (index === -1) {
+    return null;
+  }
+  contacts[index] = { id, ...data };
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return contacts[index];
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+};
